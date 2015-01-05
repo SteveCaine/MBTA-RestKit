323461e803e5a80d07d92f8711691cf006c3d2cf
Steve Caine <scaine@mac.com>
2015-01-05 00:00:20 -0500

    Initial Commit

---
+++ MBTA-APIs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MBTA-APIs
+//
+//  Created by Steve Caine on 01/05/15.
+//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MBTA-APIs/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  MBTA-APIs
+//
+//  Created by Steve Caine on 01/05/15.
+//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
