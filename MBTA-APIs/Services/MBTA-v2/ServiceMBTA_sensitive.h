3efda83642558687a78d72ca5721b5a8148a0a6a
Steve Caine <scaine@mac.com>
2015-01-05 19:31:20 -0500

    prep source to keep my API key out of GIT

---
+++ MBTA-APIs/Services/MBTA-v2/ServiceMBTA_sensitive.h
@@ -0,0 +1,14 @@
+//
+//  ServiceMBTA_sensitive.h
+//  MBTA-APIs
+//
+//  Created by Steve Caine on 01/05/115.
+//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//
+
+// your private key to the MBTA-v2 API goes here
+
+#define key_MBTA_v2_API	@""
+
+// you should not commit any version of this file that contains the key
+// to any source-control repository that will be made public or otherwise shared
